36a192b94ae4e9f7dac4d688c6525e1bbc227473
白超中 <1716239648@qq.com>
2021-11-10 11:28:50 +0800

    first commit

---
+++ app/src/main/java/com/bawei/bcz/StringsUtils.java
@@ -0,0 +1,16 @@
+package com.bawei.bcz;
+
+/**
+ * 作者：Bai-CZ on 2021/11/10 11:23
+ * <p>
+ * 邮箱：1716239648@qq.com
+ */
+public class StringsUtils {
+    public   static  boolean isStrings(String s){
+        if(s!=null||s.equals("")){
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
